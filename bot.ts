cd80a552e3ad8278fa6cdb3fc7c4c38748b31851
MKHL <mishaok@gmail.com>
2022-05-16 01:50:05 +0300

    another test commit

--- bot.ts
+++ bot.ts
@@ -190,6 +190,58 @@ client.on('interactionCreate', async (interaction) => {
 +----------------------------------+---------+------------------------+----------------+
 `
 +'```')
+interaction.reply('```'+
+`
++----------------------------------+---------+------------------------+----------------+
+|               Col1               |  Col2   |          Col3          | Numeric Column |
++----------------------------------+---------+------------------------+----------------+
+| Value 1                          | Value 2 | 123                    |           10.0 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| This is a row with only one cell |         |                        |                |
++----------------------------------+---------+------------------------+----------------+
+`
++'```')
+interaction.reply('```'+
+`
++----------------------------------+---------+------------------------+----------------+
+|               Col1               |  Col2   |          Col3          | Numeric Column |
++----------------------------------+---------+------------------------+----------------+
+| Value 1                          | Value 2 | 123                    |           10.0 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| Separate                         | cols    | with a tab or 4 spaces |       -2,027.1 |
+| This is a row with only one cell |         |                        |                |
++----------------------------------+---------+------------------------+----------------+
+`
++'```')
             break
         case 'help':
             interaction.reply('HELLO, NO HELP AVAILABLE;)')
