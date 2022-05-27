3a8b38d29b816ffe410be666e279ae5516bb6f41
MKHL <mishaok@gmail.com>
2022-05-27 15:10:20 +0300

    15 RV1Q5 TEST

--- bot.ts
+++ bot.ts
@@ -747,7 +747,8 @@ let X = Denom.div(gcdNumDen)
 let multiplicator1 = Big(1000000).div(X).round(0,Big.roundDown)
 let multiplicator2 = Big(100000).div(X).round(0,Big.roundDown)
 if (multiplicator1.gt(0)) {
-    let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)   
+    let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)
+    if (mult.eq(0)) {mult = Big(1)}  
     S = S.times(mult)
     X = X.times(mult)
 }
@@ -794,7 +795,8 @@ functions[15]['RV1Q2'] = function () {
     let multiplicator1 = Big(1000000).div(X).round(0,Big.roundDown)
     let multiplicator2 = Big(100000).div(X).round(0,Big.roundDown)
     if (multiplicator1.gt(0)) {
-        let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)   
+        let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)
+        if (mult.eq(0)) {mult = Big(1)}     
         S = S.times(mult)
         X = X.times(mult)
     }
@@ -840,7 +842,8 @@ functions[15]['RV1Q3'] = function () {
     let multiplicator1 = Big(1000000).div(X).round(0,Big.roundDown)
     let multiplicator2 = Big(100000).div(X).round(0,Big.roundDown)
     if (multiplicator1.gt(0)) {
-        let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)   
+        let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown) 
+        if (mult.eq(0)) {mult = Big(1)}    
         S = S.times(mult)
         X = X.times(mult)
     }
@@ -886,7 +889,8 @@ functions[15]['RV1Q4'] = function () {
     let multiplicator1 = Big(1000000).div(X).round(0,Big.roundDown)
     let multiplicator2 = Big(100000).div(X).round(0,Big.roundDown)
     if (multiplicator1.gt(0)) {
-        let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)   
+        let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)
+        if (mult.eq(0)) {mult = Big(1)}     
         S = S.times(mult)
         X = X.times(mult)
     }
@@ -915,11 +919,54 @@ functions[15]['RV1Q4'].URL = 'YOUTUBE RV1Q4 URL'
 functions[15]['RV1Q4'].is_text = 1
 
 
+functions[15]['RV1Q5'] = function () {
+    let R1000mas = [Big(1010), Big(1020), Big(1030), Big(1040), Big(1050), Big(1060), Big(1070), Big(1080), Big(1090), Big(1100), Big(1110), Big(1120), Big(1125), Big(1130), Big(1140), Big(1150), Big(1200), Big(1250),]
+    let R1000 = R1000mas[Math.floor(Math.random() * (R1000mas.length))]
+    let DigitsMas = [Big(0),Big(1),Big(2),Big(3),Big(4),Big(5),Big(6),Big(7),Big(8),Big(9),]
+    let Nmas = [Big(2),Big(3), Big(4),] // Большее число строк заебешься считать
+    let N = Nmas[Math.floor(Math.random() * (Nmas.length))] //Получили кол-во равных выплат
+    
+    let Denom = R1000.pow(N.toNumber())
+    let Numer = Big(0)
+    for (let i = 0; i < N; i++) {
+        Numer = Numer.plus(Big(1000).pow(N.toNumber()-i).times(R1000.pow(i)))
+    }
+    let gcdNumDen = gcdBIG(Numer, Denom)
+    let S = Numer.div(gcdNumDen)
+    let X = Denom.div(gcdNumDen)
+    let multiplicator1 = Big(1000000).div(X).round(0,Big.roundDown)
+    let multiplicator2 = Big(100000).div(X).round(0,Big.roundDown)
+    if (multiplicator1.gt(0)) {
+        let mult = Big(Math.random()).times(multiplicator1.plus(1).minus(multiplicator2)).plus(multiplicator2).round(0,Big.roundDown)
+        if (mult.eq(0)) {mult = Big(1)}  
+        S = S.times(mult)
+        X = X.times(mult)
+    }
+    let ANS = S.toNumber()
+    let part: string = ''
+    let dateTime = new Date()
+    
+    part +='В июле  '
+    part += (dateTime.getFullYear() + Math.floor(Math.random() * 4)) 
+    part += ' года планируется взять кредит на некоторую сумму. Условия возврата таковы:\n'
+    part += '— в январе каждого года долг увеличивается на '
+    part += R1000.div(10).minus(100).toString()
+    part += '% по сравнению с предыдущим годом;\n'
+    part += '— с февраля по июнь нужно выплатить часть долга одним платежом.\n'
+    
+    part += 'Определите, на какую сумму взяли кредит в банке, если известно, что кредит был выплачен '
+    part += N.toString()
+    part += ' равными платежами (то есть за '
+    part += N.toString()
+    part += ' года) и общая сумма выплат на '
+    part += X.times(N).minus(S).toString()
+    part += ' рублей больше суммы взятого кредита.'
 
 
-
-
-
+return [part, ANS]
+}
+functions[15]['RV1Q5'].URL = 'YOUTUBE RV1Q5 URL'
+functions[15]['RV1Q5'].is_text = 1
 
 
 
@@ -1048,6 +1095,7 @@ client.on('interactionCreate', async (interaction) => {
 | RV1Q2 | Равные выплаты. Обычная. То же, что RV1Q1, только в условии не X, а сумма X |
 | RV1Q3 | Равные выплаты. Обычная. Теперь надо найти выплату X, зная S                |
 | RV1Q4 | Равные выплаты. Обычная. То же, что RV1Q3, только теперь в ответ сумму X    |
+| RV1Q5 | Равные выплаты. Обычная. То же, что RV1Q1, только знаем NX-S, а не просто X |
 +-------+-----------------------------------------------------------------------------+                        
 ` +'```' )
                         break
